fbb53d4c2a400b7b6979abf51ba5d6443b85ec48
Robin Morawetz <robin@nas.heimnetzwerk>
2014-04-12 01:34:37 +0200

    completely class based now, nice course list and course details

--- ysnp/urls.py
+++ ysnp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
-from .views import Home, ListCourseView
+from .views import Home, CourseListView, CourseDetailView
 
 admin.autodiscover()
 
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', login, {'template_name': 'login.html'}, name='user-login'),
     url(r'^logout/$', logout, {'template_name': 'logout.html'}, name='user-logout'),
-    url(r'^courses/$', ListCourseView.as_view(), name='courses-list'),
-    url(r'^course/(?P<course_id>\d+)/','ysnp.views.course_view', name='course-detail')
+    url(r'^courses/$', CourseListView.as_view(), name='course-list'),
+    url(r'^courses/(?P<pk>[0-9]+)/$',CourseDetailView.as_view(), name='course-detail')
 )
--- ysnp/views.py
+++ ysnp/views.py
@@ -3,25 +3,34 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.template import RequestContext, Template
-from django.views.generic import ListView, TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
 from braces.views import LoginRequiredMixin
-from ysnp.models import Course,Student
+from ysnp.models import Course,Student, Student_Course
 
 class Home(LoginRequiredMixin, TemplateView):
     template_name = 'base.html'
     
-class ListCourseView(LoginRequiredMixin, ListView):
-
+class CourseListView(LoginRequiredMixin, ListView):
     model = Course
-    template_name = 'courses_list.html'
+    template_name = 'course_list.html'
+    context_object_name = 'courses'
+    
+    def get_context_data(self, **kwargs):
+        context = super(CourseListView, self).get_context_data(**kwargs)
+        for course in self.object_list:
+            course.attendees = Student.objects.filter(student_course__course=course).count()
+        return context
 
+class CourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = 'course_detail.html'
+    context_object_name = 'course'
+    
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['students'] = Student.objects.filter(student_course__course__courseId=self.kwargs.get('pk'))
+        for student in context['students']:
+            student.semester = Student_Course.objects.get(course=self.object, matrikelNr=student).semester
+        return context
 
-@login_required
-def course_view(request, id):
-    if id:
-        students = Student.objects.filter(student_course__course=id)
-        return render(request, 'studentList.html', {'students':students})
-        
-        
-        
         
